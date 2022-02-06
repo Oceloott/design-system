747d93c5aadd6cbf0062a79d9a947ce2300d577d
RobinBas <basrobin77@gmail.com>
2022-02-06 17:11:18 +0100

    form input

---
+++ components/FormInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <input :type="type" :name="name" :placeholder="placeholder" >
+</template>
+
+<script>
+    //--text-color-primary-black
+export default {
+    name: "FormText",
+    props: {
+        placeholder: String,
+        name: { type : String, default: "input"},
+        type: { type : String, default: "text"},
+        //classes: { type: String},
+        //color: { type: String, default: "primary-black"}
+    },
+    computed: {
+        
+    }
+}
+</script>
